d49acabbf2fadc5891500aee61e89035567cff23
Alexigho <alexbriwen@gmail.com>
2022-06-30 00:53:21 -0700

    Task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - prints the string in the put function
+ * Return: 0 if success
+ */
+int main(void)
+{
+puts("\ "Programming is like building a multilingual puzzle");
+return (0);
+}
